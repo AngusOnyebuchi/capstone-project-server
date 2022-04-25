42cf80da6c751c41ff7ab55ef5fc6ac4b74cc9b2
AngusOnyebuchi <66187866+AngusOnyebuchi@users.noreply.github.com>
2022-04-25 10:52:20 +0200

    first commit

---
+++ .history/server_20220423205349.js
@@ -0,0 +1,44 @@
+const http = require('http')
+
+const fs = require('fs')
+const os = require('os')
+
+const host = '127.0.0.1'
+const port = '5000'
+
+const server = http.createServer((req,res)=> {
+    const url = req.url
+
+    if (url === '/') {
+        fs.readFile('./pages/index.html', function(err, data) {
+            res.writeHead(200, {'Content-type': 'text/html'})
+            res.write(data)
+            return res.end()
+        })
+    } 
+    else if (url === '/about') {
+        fs.readFile('./pages/about.html', function(err, data) {
+            res.writeHead(200, {'Content-type': 'text/html'})
+            res.write(data)
+            return res.end()
+        })
+    }
+    else if (url === '/sys') {
+        fs.readFile('./pages/index.html', function(err, data) {
+            res.writeHead(201, {'Content-type': 'text/plain'})
+            res.write(data)
+            return res.end()
+        })
+    }
+    else {
+        fs.readFile('./pages/404.html', function(err, data) {
+            res.writeHead(404, {'Content-type': 'text/html'})
+            res.write(data)
+            return res.end()
+        })
+    }
+})
+
+server.listen(port, host, () => {
+    console.log(`"Running at ${host}:${port}"`)
+})
